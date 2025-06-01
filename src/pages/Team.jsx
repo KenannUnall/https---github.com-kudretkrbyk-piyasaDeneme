da9214aa322dc4ef4e6502efd42d048e43d68873
kudretkrbyk <kudretkrbyk@gmail.com>
2025-06-01 15:52:54 +0300

    added responsive

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -12,22 +12,22 @@ import React, { useState } from "react";
 const Team = () => {
   const teamMembers = [
     {
-      name: "Michael Johnson",
+      name: "1Michael Johnson1",
       role: "Developer",
       img: "../../src/assets/images/post/imgpost1.jpg",
     },
     {
-      name: "Nathaniel Lewis",
+      name: "2 Lewis",
       role: "Founder & CO",
       img: "../../src/assets/images/post/imgpost2.jpg",
     },
     {
-      name: "Timothy Young",
+      name: "3Timothy Young",
       role: "Designer",
       img: "../../src/assets/images/post/imgpost3.jpg",
     },
     {
-      name: "David Williams",
+      name: "4David Williams",
       role: "Consultant",
       img: "../../src/assets/images/post/imgpost4.jpg",
     },
@@ -36,122 +36,149 @@ const Team = () => {
   const [hovered, setHovered] = useState(null);
 
   return (
-    <section className="relative pt-[130px] bg-[#0f1928]">
-      <div className="container mx-auto px-4">
-        <div className="text-center mb-16">
-          <h5 className="text-[#14C2A3] uppercase font-bold font-chakra tracking-wide mb-2">
-            Our Team
-          </h5>
-          <h2 className="text-white text-3xl md:text-4xl font-bold font-chakra">
-            Meet our skilled team
-          </h2>
-        </div>
+    <div className="bg-[#0c1226] min-h-screen w-full text-white font-chakra relative overflow-visible">
+      <section className="relative py-10 text-center bg-[#141A31] group">
+        <h1 className="text-5xl md:text-6xl font-bold  tracking-wider text-white">
+          Ekibimiz
+        </h1>
+        <div className=" w-24 h-1 group-hover:w-48 duration-500 bg-[#14C2A3] mx-auto"></div>
+      </section>{" "}
+      <section className="relative  pt-10 bg-[#0f1928]">
+        <div className="container mx-auto px-4">
+          <div className="text-center mb-16">
+            <h5 className="text-[#14C2A3] uppercase font-bold font-chakra tracking-wide mb-2">
+              Our Team
+            </h5>
+            <h2 className="text-white text-3xl md:text-4xl font-bold font-chakra">
+              Meet our skilled team
+            </h2>
+          </div>
 
-        <div className="flex flex-wrap border border-[#2c3242] rounded-[40px] overflow-hidden">
-          {teamMembers.map((member, index) => (
-            <div
-              key={index}
-              className={`w-full sm:w-1/2 lg:w-1/4 border-r border-[#2c3242] ${
-                index >= 2 ? "border-t" : ""
-              }`}
-              onMouseEnter={() => setHovered(index)}
-              onMouseLeave={() => setHovered(null)}
-            >
-              <div className="h-full group text-center  relative transition duration-300">
-                <div className="bg-[#13182b]  rounded-3xl p-6 h-full">
-                  <img
-                    src={member.img}
-                    alt={member.name}
-                    className="w-40 h-40 rounded-full mx-auto object-cover mb-6"
-                  />
-                  <h3 className="text-white text-lg font-semibold font-chakra mb-1">
-                    {member.name}
-                  </h3>
-                  <p className="text-[#b3b3c0] text-sm mb-6 font-medium font-chakra">
-                    {member.role}
-                  </p>
+          <div className="flex flex-wrap border border-[#2c3242] rounded-[40px] overflow-hidden">
+            {teamMembers.map((member, index) => {
+              const total = teamMembers.length;
 
-                  <div className="flex justify-center items-center gap-10 relative">
-                    {/* Left */}
-                    <ul className="flex gap-3 opacity-0 group-hover:opacity-100 transform transition duration-300 translate-x-10 group-hover:translate-x-0">
-                      <li>
-                        <a
-                          href="#"
-                          className="w-8 h-8 bg-[#13182b] rounded-full flex items-center justify-center  transition"
+              // responsive row kontrolü
+              const isLastRowDefault = index === total - 1;
+              const isLastRowSm = index >= total - 2;
+              const isLastRowLg = index >= total - 4;
+
+              // responsive col kontrolü
+              const isLastColSm = (index + 1) % 2 === 0;
+              const isLastColLg = (index + 1) % 4 === 0;
+
+              return (
+                <div
+                  key={index}
+                  className={`
+        w-full sm:w-1/2 lg:w-1/4 
+        border-[#2c3242]
+        ${!isLastColLg ? "lg:border-r" : ""}
+        ${!isLastColSm ? "sm:border-r" : ""}
+        ${!isLastRowLg ? "lg:border-b" : ""}
+        ${!isLastRowSm ? "sm:border-b" : ""}
+        ${!isLastRowDefault ? "border-b" : ""}
+      `}
+                  onMouseEnter={() => setHovered(index)}
+                  onMouseLeave={() => setHovered(null)}
+                >
+                  <div className="h-full group text-center relative transition duration-300">
+                    <div className="bg-[#13182b] rounded-3xl p-6 h-full">
+                      <img
+                        src={member.img}
+                        alt={member.name}
+                        className="w-40 h-40 rounded-full mx-auto object-cover mb-6"
+                      />
+                      <h3 className="text-white text-lg font-semibold font-chakra mb-1">
+                        {member.name}
+                      </h3>
+                      <p className="text-[#b3b3c0] text-sm mb-6 font-medium font-chakra">
+                        {member.role}
+                      </p>
+
+                      <div className="flex justify-center items-center gap-10 relative">
+                        {/* Left */}
+                        <ul className="flex gap-3 opacity-0 group-hover:opacity-100 transform transition duration-300 translate-x-10 group-hover:translate-x-0">
+                          <li>
+                            <a
+                              href="#"
+                              className="w-8 h-8 bg-[#13182b] rounded-full flex items-center justify-center transition"
+                            >
+                              <FaSquareXTwitter className="text-white hover:text-[#14C2A3] size-5 transition duration-300" />
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              className="w-8 h-8 bg-[#13182b] rounded-full flex items-center justify-center transition"
+                            >
+                              <FaSquareFacebook className="text-white hover:text-[#14C2A3] size-5 transition duration-300" />
+                            </a>
+                          </li>
+                        </ul>
+
+                        {/* Toggle Icon */}
+                        <span
+                          className={`w-8 h-8 flex items-center justify-center rounded-full text-white absolute left-1/2 -translate-x-1/2 transition-all duration-[500ms] ${
+                            hovered === index
+                              ? "opacity-0 rotate-180"
+                              : "opacity-100"
+                          }`}
                         >
-                          <FaSquareXTwitter className="text-white hover:text-[#14C2A3] size-5 transform transition duration-300" />
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#"
-                          className="w-8 h-8 bg-[#13182b] rounded-full flex items-center justify-center  transition"
-                        >
-                          <FaSquareFacebook className="text-white hover:text-[#14C2A3] size-5 transform transition duration-300" />
-                        </a>
-                      </li>
-                    </ul>
+                          <FaPlus />
+                        </span>
 
-                    {/* Toggle Icon */}
-                    <span
-                      className={`w-8 h-8  flex items-center justify-center rounded-full   text-white absolute left-1/2 -translate-x-1/2 transition-all  duration-[500ms] ${
-                        hovered === index
-                          ? "opacity-0 rotate-180"
-                          : "opacity-100"
-                      }`}
-                    >
-                      <FaPlus />
-                    </span>
+                        {hovered === index && (
+                          <span className="w-8 h-8 flex items-center justify-center text-white absolute left-1/2 -translate-x-1/2 transition-all duration-300">
+                            <FaMinus className="size-5" />
+                          </span>
+                        )}
 
-                    {hovered === index && (
-                      <span className="w-8 h-8  flex items-center justify-center text-white absolute left-1/2 -translate-x-1/2  transition-all duration-300">
-                        <FaMinus className="size-5" />
-                      </span>
-                    )}
-
-                    {/* Right */}
-                    <ul className="flex gap-3 opacity-0 group-hover:opacity-100 transform transition duration-300 -translate-x-10 group-hover:translate-x-0">
-                      <li>
-                        <a
-                          href="#"
-                          className="w-8 h-8 bg-[#13182b] rounded-full flex items-center justify-center  transition"
-                        >
-                          <FaLinkedin className="text-white hover:text-[#14C2A3] size-5 transform transition duration-300" />
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#"
-                          className="w-8 h-8 bg-[#13182b] rounded-full flex items-center justify-center  transition"
-                        >
-                          <FaInstagram className="text-white hover:text-[#14C2A3] size-5 transform transition duration-300" />
-                        </a>
-                      </li>
-                    </ul>
+                        {/* Right */}
+                        <ul className="flex gap-3 opacity-0 group-hover:opacity-100 transform transition duration-300 -translate-x-10 group-hover:translate-x-0">
+                          <li>
+                            <a
+                              href="#"
+                              className="w-8 h-8 bg-[#13182b] rounded-full flex items-center justify-center transition"
+                            >
+                              <FaLinkedin className="text-white hover:text-[#14C2A3] size-5 transition duration-300" />
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              className="w-8 h-8 bg-[#13182b] rounded-full flex items-center justify-center transition"
+                            >
+                              <FaInstagram className="text-white hover:text-[#14C2A3] size-5 transition duration-300" />
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          ))}
-        </div>
+              );
+            })}
+          </div>
 
-        {/* Background shapes */}
-        {/* <div className="absolute top-0 left-0 -z-10">
+          {/* Background shapes */}
+          {/* <div className="absolute top-0 left-0 -z-10">
           <img
             src="/_next/static/media/t_shape1.b28cbf28.png"
             alt="shape1"
             className="max-w-none"
           />
         </div> */}
-        {/* <div className="absolute top-10 right-0 -z-10">
+          {/* <div className="absolute top-10 right-0 -z-10">
           <img
             src="/_next/static/media/t_shape2.b6252d87.png"
             alt="shape2"
             className="max-w-none"
           />
         </div> */}
-      </div>
-    </section>
+        </div>
+      </section>
+    </div>
   );
 };
 
